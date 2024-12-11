9b83d04bbd39d800276dd591c35ca9aad5ec19fc
SelligtomGit86 <thomas.delafaye@serli.com>
2024-12-11 15:21:48 +0100

    build otoroshi-biscuit-studio documentation website

--- docs/assets/js/acecf23e.df33216f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/otoroshi-biscuit-studio/blog","blogTitle":"Blog","authorsListPath":"/otoroshi-biscuit-studio/blog/authors"}')}}]);
--- docs/assets/js/b59e956a.efab7a72.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[879],{2079:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/otoroshi-biscuit-studio/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
