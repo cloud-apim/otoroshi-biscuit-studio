9e02b97b5a924e18c0ed67df7bd22a2beb4a5ed9
SelligtomGit86 <thomas.delafaye@serli.com>
2024-12-11 15:15:58 +0100

    build otoroshi-biscuit-studio documentation website

--- build-doc.sh
+++ build-doc.sh
@@ -1,9 +1,10 @@
 cd ./documentation
+rm -rf ./node-modules
 npm install
 npm run build
 cd ..
 rm -rf ./docs
 mv ./documentation/build ./docs 
 git add --all
-git commit -am 'build documentation website'
+git commit -am 'build otoroshi-biscuit-studio documentation website'
 git push origin main
---
+++ docs/assets/js/b59e956a.efab7a72.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[879],{2079:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/otoroshi-biscuit-studio/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
