bd7390b7c0b9b47cf5fcb2e3efd7f78cae1fac8c
SelligtomGit86 <thomas.delafaye@serli.com>
2024-12-11 15:26:12 +0100

    format JS files

--- src/main/resources/cloudapim/extensions/biscuit/BiscuitAttenuatorPage.js
+++ src/main/resources/cloudapim/extensions/biscuit/BiscuitAttenuatorPage.js
@@ -1,112 +1,127 @@
 class BiscuitAttenuatorPage extends Component {
-  formSchema = {
-    _loc: {
-      type: 'location',
-      props: {},
-    },
-    id: { type: 'string', disabled: true, props: { label: 'Id', placeholder: '---' } },
-    name: {
-      type: 'string',
-      props: { label: 'Name', placeholder: 'My Awesome Context' },
-    },
-    description: {
-      type: 'string',
-      props: { label: 'Description', placeholder: 'Description of the Context' },
-    },
-    metadata: {
-      type: 'object',
-      props: { label: 'Metadata' },
-    },
-    tags: {
-      type: 'array',
-      props: { label: 'Tags' },
-    },
-     keypair_ref: {
-        type: 'select',
-        props: {
-            label: 'Key Pair Reference',
-            valuesFrom: '/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-keypairs',
-            transformer: (item) => ({ label: item.name, value: item.id }),
-        }
-    },
-    config : {
-        type: 'jsonobjectcode',
-        props: { label: 'Configuration' }
-    }
-  };
+	formSchema = {
+		_loc: {
+			type: "location",
+			props: {},
+		},
+		id: {
+			type: "string",
+			disabled: true,
+			props: { label: "Id", placeholder: "---" },
+		},
+		name: {
+			type: "string",
+			props: { label: "Name", placeholder: "My Awesome Context" },
+		},
+		description: {
+			type: "string",
+			props: {
+				label: "Description",
+				placeholder: "Description of the Context",
+			},
+		},
+		metadata: {
+			type: "object",
+			props: { label: "Metadata" },
+		},
+		tags: {
+			type: "array",
+			props: { label: "Tags" },
+		},
+		keypair_ref: {
+			type: "select",
+			props: {
+				label: "Key Pair Reference",
+				valuesFrom:
+					"/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-keypairs",
+				transformer: (item) => ({ label: item.name, value: item.id }),
+			},
+		},
+		config: {
+			type: "jsonobjectcode",
+			props: { label: "Configuration" },
+		},
+	};
 
-  columns = [
-    {
-      title: 'Name',
-      filterId: 'name',
-      content: (item) => item.name,
-    },
-    {
-      title: 'Description',
-      filterId: 'description',
-      content: (item) => item.description,
-    }
-  ];
+	columns = [
+		{
+			title: "Name",
+			filterId: "name",
+			content: (item) => item.name,
+		},
+		{
+			title: "Description",
+			filterId: "description",
+			content: (item) => item.description,
+		},
+	];
 
-  formFlow = [
-    '_loc',
-    'id',
-    'name',
-    'description',
-    'tags',
-    'metadata',
-    'keypair_ref',
-    'config'
-  ];
+	formFlow = [
+		"_loc",
+		"id",
+		"name",
+		"description",
+		"tags",
+		"metadata",
+		"keypair_ref",
+		"config",
+	];
 
-  componentDidMount() {
-    this.props.setTitle(`Biscuit Attenuators`);
-  }
+	componentDidMount() {
+		this.props.setTitle(`Biscuit Attenuators`);
+	}
 
-  client = BackOfficeServices.apisClient('biscuit.extensions.cloud-apim.com', 'v1', 'biscuit-attenuators');
+	client = BackOfficeServices.apisClient(
+		"biscuit.extensions.cloud-apim.com",
+		"v1",
+		"biscuit-attenuators"
+	);
 
-  render() {
-    return (
-      React.createElement(Table, {
-        parentProps: this.props,
-        selfUrl: "extensions/cloud-apim/biscuit/attenuators",
-        defaultTitle: "All Biscuit Verifiers",
-        defaultValue: () => ({
-          id: 'biscuit_attenuator_' + uuid(),
-          name: 'Biscuit Attenuator',
-          description: 'A simple Biscuit Attenuator',
-          tags: [],
-          metadata: {},
-          keypair_ref: '',
-          config: {
-            "checks": [],
-            "facts": [],
-            "resources": [],
-            "rules": [],
-            "revocation_ids": []
-          }
-        }),
-        itemName: "Biscuit Attenuator",
-        formSchema: this.formSchema,
-        formFlow: this.formFlow,
-        columns: this.columns,
-        stayAfterSave: true,
-        fetchTemplate: () => this.client.template(),
-        fetchItems: (paginationState) => this.client.findAll(),
-        updateItem: this.client.update,
-        deleteItem: this.client.delete,
-        createItem: this.client.create,
-        navigateTo: (item) => {
-          window.location = `/bo/dashboard/extensions/cloud-apim/biscuit/attenuators/edit/${item.id}`
-        },
-        itemUrl: (item) => `/bo/dashboard/extensions/cloud-apim/biscuit/attenuators/edit/${item.id}`,
-        showActions: true,
-        showLink: true,
-        rowNavigation: true,
-        extractKey: (item) => item.id,
-        export: true,
-        kubernetesKind: "BiscuitAttenuator"
-      }, null)
-    );
-  }
+	render() {
+		return React.createElement(
+			Table,
+			{
+				parentProps: this.props,
+				selfUrl: "extensions/cloud-apim/biscuit/attenuators",
+				defaultTitle: "All Biscuit Verifiers",
+				defaultValue: () => ({
+					id: "biscuit_attenuator_" + uuid(),
+					name: "Biscuit Attenuator",
+					description: "A simple Biscuit Attenuator",
+					tags: [],
+					metadata: {},
+					keypair_ref: "",
+					config: {
+						checks: [],
+						facts: [],
+						resources: [],
+						rules: [],
+						revocation_ids: [],
+					},
+				}),
+				itemName: "Biscuit Attenuator",
+				formSchema: this.formSchema,
+				formFlow: this.formFlow,
+				columns: this.columns,
+				stayAfterSave: true,
+				fetchTemplate: () => this.client.template(),
+				fetchItems: (paginationState) => this.client.findAll(),
+				updateItem: this.client.update,
+				deleteItem: this.client.delete,
+				createItem: this.client.create,
+				navigateTo: (item) => {
+					window.location = `/bo/dashboard/extensions/cloud-apim/biscuit/attenuators/edit/${item.id}`;
+				},
+				itemUrl: (item) =>
+					`/bo/dashboard/extensions/cloud-apim/biscuit/attenuators/edit/${item.id}`,
+				showActions: true,
+				showLink: true,
+				rowNavigation: true,
+				extractKey: (item) => item.id,
+				export: true,
+				kubernetesKind: "BiscuitAttenuator",
+			},
+			null
+		);
+	}
 }
