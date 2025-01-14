0456b222befb138c281a72551b67a798e6bf2afe
SelligtomGit86 <thomas.delafaye@serli.com>
2025-01-14 09:53:21 +0100

    update documentation

--- build-doc.sh
+++ build-doc.sh
@@ -5,6 +5,7 @@ npm run build
 cd ..
 rm -rf ./docs
 mv ./documentation/build ./docs 
+git add ./documentation
 git add ./docs
 git commit -m 'build otoroshi-biscuit-studio documentation website'
 git push origin main
