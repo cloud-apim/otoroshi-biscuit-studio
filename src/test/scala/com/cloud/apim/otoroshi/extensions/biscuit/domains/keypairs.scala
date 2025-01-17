a46f0bd268d3b64ddb331ecd04ac52d100c1d75c
SelligtomGit86 <thomas.delafaye@serli.com>
2025-01-17 12:52:18 +0100

    update tests

--- src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/domains/keypairs.scala
+++ src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/domains/keypairs.scala
@@ -16,8 +16,8 @@ object BiscuitKeyPairsUtils extends Assertions {
     assert(keypairRes.created, s"[${keypair.id}] keypair has not been created")
     assert(keypairRes.resp.json.at("pubKey").isDefined, s"[${keypair.id}] keypair public key is missing")
     assert(keypairRes.resp.json.at("privKey").isDefined, s"[${keypair.id}] keypair private key is missing")
-    assertEquals(keypairRes.resp.json.at("id").asOpt[String].getOrElse(""), "biscuit-keypair_97930360-d343-461d-98d1-8505b5ccf2f0", "keypair id not matching with provided id")
-    assertEquals(keypairRes.resp.json.at("pubKey").asOpt[String].getOrElse(""), "CA31F831E8A750EF0E9C5F8B3D0CA1265428BFA9CB506CFCF4B8E883168B13C8", s"public key is not same as provided")
-    assertEquals(keypairRes.resp.json.at("privKey").asOpt[String].getOrElse(""), "B09F278A4E37DBCC0AC9C10F941DFB90B5A59D3678C0B33ED76C47F760E57DB2", s"private key is not same as provided")
+    assertEquals(keypairRes.resp.json.at("id").asOpt[String].getOrElse(""), keypair.id, "keypair id not matching with provided id")
+    assertEquals(keypairRes.resp.json.at("pubKey").asOpt[String].getOrElse(""), keypair.pubKey, s"public key is not same as provided")
+    assertEquals(keypairRes.resp.json.at("privKey").asOpt[String].getOrElse(""), keypair.privKey, s"private key is not same as provided")
   }
 }
--- src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/verifiers.scala
+++ src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/verifiers.scala
@@ -6,11 +6,375 @@ import com.cloud.apim.otoroshi.extensions.biscuit.entities.{BiscuitKeyPair, Bisc
 import com.cloud.apim.otoroshi.extensions.biscuit.{BiscuitExtensionSuite, OtoroshiClient}
 import otoroshi.api.Otoroshi
 import otoroshi.models.EntityLocation
-import otoroshi.utils.syntax.implicits.BetterSyntax
+import otoroshi.utils.syntax.implicits._
+import play.api.libs.json.{JsObject, Json}
 
+import java.util.UUID
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 class TestVerifiers extends BiscuitExtensionSuite {
+
+  def testBiscuitVerifier(client: OtoroshiClient, awaitFor: FiniteDuration)(implicit ec: ExecutionContext, mat: Materializer): Unit = {
+    val port = client.port
+
+    // user("biscuit-studio-test");
+    // role("user");
+    val encodedToken = "EpYBCiwKE2Jpc2N1aXQtc3R1ZGlvLXRlc3QYAyIJCgcIChIDGIAIIggKBggGEgIYChIkCAASIIjCXLU5A-JBhCzBWklpKOr4azUhMLMXQzrhcLXTqxsfGkA6aVZxS-uu8tpQtbK_NxeaGxpc-WPBYPgO83NZQZSdLbE4ELcfgn-6OoH-jp6Ych7M_T3t1vBoNnSp4Paah9UHIiIKIBvwbLEZSZUtv2sQCY_UUBI-wBjBk9gnHXW4uQRGHzSv"
+
+    val keypairID = s"biscuit-keypair_53105273-61db-4791-a8cf-04e98b5a2c12"
+    val verifierId = s"biscuit-verifier_603ad95d-b723-4883-817a-f9c739e157b8"
+    val routeVerifierId = s"route_${UUID.randomUUID().toString}"
+
+    val demoKeyPair = BiscuitKeyPair(
+      id = keypairID,
+      name = "New Biscuit Key Pair",
+      description = "New biscuit KeyPair",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      pubKey = "0D6C7CBAECBA63916D7CAE5981C411EAF6A18929910709977FFDDFBD4433EF27",
+      privKey = "1304480d1d9eea4075de296d8d84c2522703778c01b0a1459145716a7c33a665"
+    )
+
+    BiscuitKeyPairsUtils.createKeypairEntity(client)(demoKeyPair)
+
+    val conf = VerifierConfig(
+      checks = List.empty,
+      facts = List.empty,
+      resources = List.empty,
+      rules = List.empty,
+      policies = Seq("allow if user(\"biscuit-studio-test\");"),
+      revokedIds = List.empty
+    )
+
+    val verifier = BiscuitVerifier(
+      id = verifierId,
+      name = "New Biscuit Verifier entity",
+      description = "New biscuit Verifier entity",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      keypairRef = keypairID,
+      config =  conf.some
+    )
+
+    BiscuitVerifiersUtils.createVerifierEntity(client)(verifier)
+
+    val routeWithVerifier = client.forEntity("proxy.otoroshi.io", "v1", "routes").upsertRaw(routeVerifierId, Json.parse(
+      s"""{
+         |  "id": "${routeVerifierId}",
+         |  "name": "biscuit-verifier",
+         |  "frontend": {
+         |    "domains": [
+         |      "verifier.oto.tools"
+         |    ]
+         |  },
+         |  "backend": {
+         |    "targets": [
+         |      {
+         |        "id": "target_1",
+         |        "hostname": "request.otoroshi.io",
+         |        "port": 443,
+         |        "tls": true
+         |      }
+         |    ],
+         |    "root": "/",
+         |    "rewrite": false,
+         |    "load_balancing": {
+         |      "type": "RoundRobin"
+         |    }
+         |  },
+         |   "backend_ref": null,
+         |  "plugins": [
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi.next.plugins.OverrideHost",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {},
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "transform_request": 0
+         |      }
+         |    },
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi_plugins.com.cloud.apim.otoroshi.extensions.biscuit.plugins.BiscuitTokenValidator",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {
+         |        "verifier_ref": "${verifierId}",
+         |        "rbac_ref": "",
+         |        "enable_remote_facts": false,
+         |        "remote_facts_ref": "",
+         |        "enforce": true,
+         |        "extractor_type": "header",
+         |        "extractor_name": "biscuit-header"
+         |      },
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "validate_access": 0
+         |      }
+         |    }
+         |  ]
+         |}""".stripMargin)).awaitf(awaitFor)
+    assert(routeWithVerifier.created, s"verifier route chat has not been created")
+    await(1300.millis)
+
+    val headers = Map(
+      "biscuit-header" -> encodedToken
+    )
+
+    val resp = client.call("GET", s"http://verifier.oto.tools:${port}", headers, None).awaitf(awaitFor)
+    println(resp.body)
+    assertEquals(resp.status, 200, s"verifier route did not respond with 200")
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").deleteRaw(routeVerifierId)
+    await(1300.millis)
+  }
+
+  def testBiscuitVerifierWithBadToken(client: OtoroshiClient, awaitFor: FiniteDuration)(implicit ec: ExecutionContext, mat: Materializer): Unit = {
+    val port = client.port
+
+    // user("biscuit-studio-test");
+    // role("user");
+    val encodedToken = "EcYBCiwKE2Jpc2N1aXQtc3R1ZGlvLXRlc3QYAyIJCgcIChIDGIAIIggKBggGEgIYChIkCAASIIjCXLU5A-JBhCzBWklpKOr4azUhMLMXQzrhcLXTqxsfGkA6aVZxS-uu8tpQtbK_NxeaGxpc-WPBYPgO83NZQZSdLbE4ELcfgn-6OoH-jp6Ych7M_T3t1vBoNnSp4Paah9UHIiIKIBvwbLEZSZUtv2sQCY_UUBI-wBjBk9gnHXW4uQRGHzSv"
+
+    val keypairID = s"biscuit-keypair_53105273-61db-4791-a8cf-04e98b5a2c12"
+    val verifierId = s"biscuit-verifier_603ad95d-b723-4883-817a-f9c739e157b8"
+    val routeVerifierId = s"route_${UUID.randomUUID().toString}"
+
+    val demoKeyPair = BiscuitKeyPair(
+      id = keypairID,
+      name = "New Biscuit Key Pair",
+      description = "New biscuit KeyPair",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      pubKey = "0D6C7CBAECBA63916D7CAE5981C411EAF6A18929910709977FFDDFBD4433EF27",
+      privKey = "1304480d1d9eea4075de296d8d84c2522703778c01b0a1459145716a7c33a665"
+    )
+
+    BiscuitKeyPairsUtils.createKeypairEntity(client)(demoKeyPair)
+
+    val conf = VerifierConfig(
+      checks = List.empty,
+      facts = List.empty,
+      resources = List.empty,
+      rules = List.empty,
+      policies = Seq("allow if user(\"biscuit-studio-test\");"),
+      revokedIds = List.empty
+    )
+
+    val verifier = BiscuitVerifier(
+      id = verifierId,
+      name = "New Biscuit Verifier entity",
+      description = "New biscuit Verifier entity",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      keypairRef = keypairID,
+      config =  conf.some
+    )
+
+    BiscuitVerifiersUtils.createVerifierEntity(client)(verifier)
+
+    val routeWithVerifier = client.forEntity("proxy.otoroshi.io", "v1", "routes").upsertRaw(routeVerifierId, Json.parse(
+      s"""{
+         |  "id": "${routeVerifierId}",
+         |  "name": "biscuit-verifier",
+         |  "frontend": {
+         |    "domains": [
+         |      "verifier.oto.tools"
+         |    ]
+         |  },
+         |  "backend": {
+         |    "targets": [
+         |      {
+         |        "id": "target_1",
+         |        "hostname": "request.otoroshi.io",
+         |        "port": 443,
+         |        "tls": true
+         |      }
+         |    ],
+         |    "root": "/",
+         |    "rewrite": false,
+         |    "load_balancing": {
+         |      "type": "RoundRobin"
+         |    }
+         |  },
+         |   "backend_ref": null,
+         |  "plugins": [
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi.next.plugins.OverrideHost",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {},
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "transform_request": 0
+         |      }
+         |    },
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi_plugins.com.cloud.apim.otoroshi.extensions.biscuit.plugins.BiscuitTokenValidator",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {
+         |        "verifier_ref": "${verifierId}",
+         |        "rbac_ref": "",
+         |        "enable_remote_facts": false,
+         |        "remote_facts_ref": "",
+         |        "enforce": true,
+         |        "extractor_type": "header",
+         |        "extractor_name": "biscuit-header"
+         |      },
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "validate_access": 0
+         |      }
+         |    }
+         |  ]
+         |}""".stripMargin)).awaitf(awaitFor)
+    assert(routeWithVerifier.created, s"verifier route chat has not been created")
+    await(1300.millis)
+
+    val headers = Map(
+      "biscuit-header" -> encodedToken
+    )
+
+    val resp = client.call("GET", s"http://verifier.oto.tools:${port}", headers, None).awaitf(awaitFor)
+    println(resp.body)
+    assertEquals(resp.status, 500, s"verifier route did not respond with 200")
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").deleteRaw(routeVerifierId)
+    await(1300.millis)
+  }
+
+  def testWrongBiscuitTokenWithVerifier(client: OtoroshiClient, awaitFor: FiniteDuration)(implicit ec: ExecutionContext, mat: Materializer): Unit = {
+    val port = client.port
+
+    // user("biscuit-studio-test");
+    // role("user");
+    val encodedToken = "EpYBCiwKE2Jpc2N1aXQtc3R1ZGlvLXRlc3QYAyIJCgcIChIDGIAIIggKBggGEgIYChIkCAASIIjCXLU5A-JBhCzBWklpKOr4azUhMLMXQzrhcLXTqxsfGkA6aVZxS-uu8tpQtbK_NxeaGxpc-WPBYPgO83NZQZSdLbE4ELcfgn-6OoH-jp6Ych7M_T3t1vBoNnSp4Paah9UHIiIKIBvwbLEZSZUtv2sQCY_UUBI-wBjBk9gnHXW4uQRGHzSv"
+
+    val keypairID = s"biscuit-keypair_9cf71a29-303e-40f2-88d0-b86805a73de0"
+    val verifierId = s"biscuit-verifier_40770e97-825c-4abc-9f92-0051fa2eff67"
+    val routeVerifierId = s"route_${UUID.randomUUID().toString}"
+
+    val demoKeyPair = BiscuitKeyPair(
+      id = keypairID,
+      name = "New Biscuit Key Pair",
+      description = "New biscuit KeyPair",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      pubKey = "0D6C7CBAECBA63916D7CAE5981C411EAF6A18929910709977FFDDFBD4433EF27",
+      privKey = "1304480d1d9eea4075de296d8d84c2522703778c01b0a1459145716a7c33a665"
+    )
+
+    BiscuitKeyPairsUtils.createKeypairEntity(client)(demoKeyPair)
+
+    val conf = VerifierConfig(
+      checks = List.empty,
+      facts = List.empty,
+      resources = List.empty,
+      rules = List.empty,
+      policies = Seq("allow if user(\"biscuit-studio-test-2\");"),
+      revokedIds = List.empty
+    )
+
+    val verifier = BiscuitVerifier(
+      id = verifierId,
+      name = "New Biscuit Verifier entity",
+      description = "New biscuit Verifier entity",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      keypairRef = keypairID,
+      config =  conf.some
+    )
+
+    BiscuitVerifiersUtils.createVerifierEntity(client)(verifier)
+
+    val routeWithVerifier = client.forEntity("proxy.otoroshi.io", "v1", "routes").upsertRaw(routeVerifierId, Json.parse(
+      s"""{
+         |  "id": "${routeVerifierId}",
+         |  "name": "biscuit-verifier-2",
+         |  "frontend": {
+         |    "domains": [
+         |      "verifier.oto.tools"
+         |    ]
+         |  },
+         |  "backend": {
+         |    "targets": [
+         |      {
+         |        "id": "target_1",
+         |        "hostname": "request.otoroshi.io",
+         |        "port": 443,
+         |        "tls": true
+         |      }
+         |    ],
+         |    "root": "/",
+         |    "rewrite": false,
+         |    "load_balancing": {
+         |      "type": "RoundRobin"
+         |    }
+         |  },
+         |   "backend_ref": null,
+         |  "plugins": [
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi.next.plugins.OverrideHost",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {},
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "transform_request": 0
+         |      }
+         |    },
+         |    {
+         |      "enabled": true,
+         |      "debug": false,
+         |      "plugin": "cp:otoroshi_plugins.com.cloud.apim.otoroshi.extensions.biscuit.plugins.BiscuitTokenValidator",
+         |      "include": [],
+         |      "exclude": [],
+         |      "config": {
+         |        "verifier_ref": "${verifierId}",
+         |        "rbac_ref": "",
+         |        "enable_remote_facts": false,
+         |        "remote_facts_ref": "",
+         |        "enforce": true,
+         |        "extractor_type": "header",
+         |        "extractor_name": "biscuit-header"
+         |      },
+         |      "bound_listeners": [],
+         |      "plugin_index": {
+         |        "validate_access": 0
+         |      }
+         |    }
+         |  ]
+         |}""".stripMargin)).awaitf(awaitFor)
+    assert(routeWithVerifier.created, s"verifier route chat has not been created")
+    await(1300.millis)
+
+    val headers = Map(
+      "biscuit-header" -> encodedToken
+    )
+
+    val resp = client.call("GET", s"http://verifier.oto.tools:${port}", headers, None).awaitf(awaitFor)
+    println(resp.body)
+    assertEquals(resp.status, 200, s"verifier route did not respond with 200")
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").deleteRaw(routeVerifierId)
+    await(1300.millis)
+  }
+
   def printHeader(str: String, what: String): Unit = {
     println("\n\n-----------------------------------------")
     println(s"  [${str}] - ${what}")
@@ -60,4 +424,19 @@ class TestVerifiers extends BiscuitExtensionSuite {
     printHeader(verifier.name, "Create new verifier entity")
     BiscuitVerifiersUtils.createVerifierEntity(client)(verifier)
   }
+
+//  test(s"create verifier plugin should be good") {
+//    printHeader(verifier.name,  "testing verifier plugin")
+//    testBiscuitVerifier(client, 30.seconds)
+//  }
+//
+//  test(s"create verifier plugin should be wrong") {
+//    printHeader("", "testing verifier plugin")
+//    testWrongBiscuitTokenWithVerifier(client, 30.seconds)
+//  }
+//
+//  test(s"create verifier plugin should be wrong") {
+//    printHeader(verifier.name,  "testing verifier plugin with bad token")
+//    testBiscuitVerifierWithBadToken(client, 30.seconds)
+//  }
 }
