30140f187891463b2c711efecf7535a19319c883
SelligtomGit86 <thomas.delafaye@serli.com>
2025-02-25 10:37:04 +0100

    update BiscuitUserExtractor

--- src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/privateappsuserextractor.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/privateappsuserextractor.scala
@@ -20,17 +20,40 @@ import scala.jdk.CollectionConverters.{asScalaBufferConverter, asScalaSetConvert
 import scala.util.{Failure, Success, Try}
 
 case class BiscuitUserExtractorConfig(
-                                      keypairRef: String = "",
-                                      enforce: Boolean = true,
-                                      extractorType: String = "header",
-                                      extractorName: String = "Authorization",
-                                      usernameKey: String = "name"
-                                    ) extends NgPluginConfig {
+  keypairRef: String = "",
+  enforce: Boolean = true,
+  extractorType: String = "header",
+  extractorName: String = "Authorization",
+  usernameKey: String = "name"
+) extends NgPluginConfig {
   def json: JsValue = BiscuitUserExtractorConfig.format.writes(this)
 }
 
 object BiscuitUserExtractorConfig {
   val configFlow: Seq[String] = Seq("keypair_ref", "enforce", "extractor_type", "extractor_name", "username_key")
+  val format = new Format[BiscuitUserExtractorConfig] {
+    override def writes(o: BiscuitUserExtractorConfig): JsValue = Json.obj(
+      "keypair_ref" -> o.keypairRef,
+      "enforce" -> o.enforce,
+      "extractor_type" -> o.extractorType,
+      "extractor_name" -> o.extractorName,
+      "username_key" -> o.usernameKey
+    )
+
+    override def reads(json: JsValue): JsResult[BiscuitUserExtractorConfig] = Try {
+      BiscuitUserExtractorConfig(
+        keypairRef = json.select("keypair_ref").asOpt[String].getOrElse(""),
+        enforce = json.select("enforce").asOpt[Boolean].getOrElse(true),
+        extractorType = json.select("extractor_type").asOpt[String].getOrElse(""),
+        extractorName = json.select("extractor_name").asOpt[String].getOrElse(""),
+        usernameKey = json.select("username_key").asOpt[String].getOrElse("")
+      )
+    } match {
+      case Failure(exception) => JsError(exception.getMessage)
+      case Success(value) => JsSuccess(value)
+    }
+  }
+
   def configSchema(name: String): Option[JsObject] = Some(Json.obj(
     "keypair_ref" -> Json.obj(
       "type" -> "select",
@@ -54,7 +77,7 @@ object BiscuitUserExtractorConfig {
       "props" -> Json.obj(
         "options" -> Seq(
           Json.obj("label" -> "Header", "value" -> "header"),
-          Json.obj("label" -> "Cookies", "value" -> "cookies"),
+          Json.obj("label" -> "Cookies", "value" -> "cookie"),
           Json.obj("label" -> "Query params", "value" -> "query")
         )
       ),
@@ -68,41 +91,30 @@ object BiscuitUserExtractorConfig {
       "label" -> "Username biscuit key"
     )
   ))
-
-  val format = new Format[BiscuitUserExtractorConfig] {
-    override def writes(o: BiscuitUserExtractorConfig): JsValue = Json.obj(
-      "keypair_ref" -> o.keypairRef,
-      "enforce" -> o.enforce,
-      "extractor_type" -> o.extractorType,
-      "extractor_name" -> o.extractorName,
-      "username_key" -> o.usernameKey
-    )
-    override def reads(json: JsValue): JsResult[BiscuitUserExtractorConfig] = Try {
-      BiscuitUserExtractorConfig(
-        keypairRef = json.select("keypair_ref").asOpt[String].getOrElse(""),
-        enforce = json.select("enforce").asOpt[Boolean].getOrElse(true),
-        extractorType = json.select("extractor_type").asOpt[String].getOrElse(""),
-        extractorName = json.select("extractor_name").asOpt[String].getOrElse(""),
-        usernameKey = json.select("username_key").asOpt[String].getOrElse("")
-      )
-    } match {
-      case Failure(exception) => JsError(exception.getMessage)
-      case Success(value) => JsSuccess(value)
-    }
-  }
 }
+
 class BiscuitUserExtractor extends NgPreRouting {
 
   private val logger = Logger("biscuit-user-extractor-plugin")
-  override def name: String = "Cloud APIM - Biscuit ApiKey bridge"
-  override def description: Option[String] = "This plugin validates a Biscuit token".some
+
+  override def name: String = "Cloud APIM - Biscuit User Extractor"
+
+  override def description: Option[String] = "This plugin extract an user from a biscuit token".some
+
   override def defaultConfigObject: Option[NgPluginConfig] = Some(BiscuitUserExtractorConfig())
+
   override def core: Boolean = false
+
   override def noJsForm: Boolean = true
+
   override def configFlow: Seq[String] = BiscuitUserExtractorConfig.configFlow
+
   override def configSchema: Option[JsObject] = BiscuitUserExtractorConfig.configSchema("biscuit-user-extractor")
+
   override def visibility: NgPluginVisibility = NgPluginVisibility.NgUserLand
+
   override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Custom("Cloud APIM"), NgPluginCategory.Custom("Biscuit Studio"), NgPluginCategory.AccessControl)
+
   override def steps: Seq[NgStep] = Seq(NgStep.ValidateAccess)
 
   override def start(env: Env): Future[Unit] = {
@@ -111,88 +123,10 @@ class BiscuitUserExtractor extends NgPreRouting {
     }
     ().vfuture
   }
-  def extractIdAndName(ctx: NgPreRoutingContext, biscuitToken: Biscuit, config: BiscuitUserExtractorConfig)(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
-    val otoroshiUserId = biscuitToken.authorizer().query("biscuit_user_id($id) <- user_id($id)")
-    val otoroshiUsername = biscuitToken.authorizer().query(s"biscuit_username($$id) <- ${config.usernameKey}($$id)")
-
-    val biscuitUserId: Option[String] = {
-      Try(otoroshiUserId).toOption
-        .map(_.asScala)
-        .flatMap(_.headOption)
-        .filter(_.name() == "biscuit_user_id")
-        .map(_.terms().asScala)
-        .flatMap(_.headOption)
-        .flatMap {
-          case str: Str => str.getValue.some
-          case _        => None
-        }
-    }
-
-    val biscuitUsername: Option[String] = {
-      Try(otoroshiUsername).toOption
-        .map(_.asScala)
-        .flatMap(_.headOption)
-        .filter(_.name() == "biscuit_username")
-        .map(_.terms().asScala)
-        .flatMap(_.headOption)
-        .flatMap {
-          case str: Str => str.getValue.some
-          case _        => None
-        }
-    }
-
-//    val clazz = classOf[UnverifiedBiscuit]
-//    Try(clazz.getField("symbols"))
-//      .orElse(Try(clazz.getDeclaredField("symbols")))
-//      .toOption
-//      .map { f =>
-//        f.setAccessible(true)
-//        f
-//      }.flatMap(v => Option(v.get(b))).map(_.asInstanceOf[SymbolTable]).foreach { symbols =>
-//        biscuitToken.authorizer().facts().facts().values().asScala.flatMap(_.asScala).foreach { fact =>
-//          val nameLng = fact.predicate().name().toInt
-//          val name = symbols.get_s(nameLng)
-//          val value: String = fact.predicate().terms().asScala.map(t => symbols.print_term(t)).mkString(", ")
-//          println(s"name: ${name} - ${value}")
-//        }
-//      }
-
-    (biscuitUserId, biscuitUsername) match {
-      case (Some(userId), Some(userName)) => {
-
-        val user: PrivateAppsUser = PrivateAppsUser(
-          randomId = IdGenerator.uuid,
-          name = userId,
-          email = userName,
-          otoroshiData = None,
-          profile= Json.obj(),
-          token = Json.obj("biscuit" -> biscuitToken.serialize_b64url()),
-          realm = s"BiscuitUserExtractor@${ctx.route.serviceDescriptor.id}",
-          authConfigId = s"BiscuitUserExtractor@${ctx.route.serviceDescriptor.id}",
-          createdAt = DateTime.now(),
-          expiredAt = DateTime.now().plusHours(1),
-          lastRefresh = DateTime.now(),
-          tags = Seq.empty,
-          metadata = Map.empty,
-          location = ctx.route.serviceDescriptor.location
-        )
-        ctx.attrs.put(otoroshi.plugins.Keys.UserKey -> user)
-        Done.right.vfuture
-      }
-      case _ => unauthorized(Json.obj("error" -> "unauthorized", "error_description" -> "Bad user extraction, user id or username not valid"))
-    }
-  }
-
-  def unauthorized(error: JsObject): Future[Either[NgPreRoutingError, Done]] = {
-    NgPreRoutingErrorWithResult(Results.Unauthorized(error)).leftf
-  }
-
-  def handleError(message: String): Future[Either[NgPreRoutingError, Done]] =
-    NgPreRoutingErrorWithResult(Results.InternalServerError(Json.obj("error" -> message))).leftf
 
   override def preRoute(
-                         ctx: NgPreRoutingContext
-                       )(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
+    ctx: NgPreRoutingContext
+  )(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
 
     val config = ctx.cachedConfig(internalName)(BiscuitUserExtractorConfig.format).getOrElse(BiscuitUserExtractorConfig())
     env.adminExtensions.extension[BiscuitExtension].flatMap(_.states.keypair(config.keypairRef)) match {
@@ -218,4 +152,83 @@ class BiscuitUserExtractor extends NgPreRouting {
       }
     }
   }
+
+  def extractIdAndName(ctx: NgPreRoutingContext, biscuitToken: Biscuit, config: BiscuitUserExtractorConfig)(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
+    val otoroshiUserId = biscuitToken.authorizer().query("biscuit_user_id($id) <- user_id($id)")
+    val otoroshiUsername = biscuitToken.authorizer().query(s"biscuit_username($$id) <- ${config.usernameKey}($$id)")
+
+    val biscuitUserId: Option[String] = {
+      Try(otoroshiUserId).toOption
+        .map(_.asScala)
+        .flatMap(_.headOption)
+        .filter(_.name() == "biscuit_user_id")
+        .map(_.terms().asScala)
+        .flatMap(_.headOption)
+        .flatMap {
+          case str: Str => str.getValue.some
+          case _ => None
+        }
+    }
+
+    val biscuitUsername: Option[String] = {
+      Try(otoroshiUsername).toOption
+        .map(_.asScala)
+        .flatMap(_.headOption)
+        .filter(_.name() == "biscuit_username")
+        .map(_.terms().asScala)
+        .flatMap(_.headOption)
+        .flatMap {
+          case str: Str => str.getValue.some
+          case _ => None
+        }
+    }
+
+    //    val clazz = classOf[UnverifiedBiscuit]
+    //    Try(clazz.getField("symbols"))
+    //      .orElse(Try(clazz.getDeclaredField("symbols")))
+    //      .toOption
+    //      .map { f =>
+    //        f.setAccessible(true)
+    //        f
+    //      }.flatMap(v => Option(v.get(b))).map(_.asInstanceOf[SymbolTable]).foreach { symbols =>
+    //        biscuitToken.authorizer().facts().facts().values().asScala.flatMap(_.asScala).foreach { fact =>
+    //          val nameLng = fact.predicate().name().toInt
+    //          val name = symbols.get_s(nameLng)
+    //          val value: String = fact.predicate().terms().asScala.map(t => symbols.print_term(t)).mkString(", ")
+    //          println(s"name: ${name} - ${value}")
+    //        }
+    //      }
+
+    (biscuitUserId, biscuitUsername) match {
+      case (Some(userId), Some(userName)) => {
+
+        val user: PrivateAppsUser = PrivateAppsUser(
+          randomId = IdGenerator.uuid,
+          name = userId,
+          email = userName,
+          otoroshiData = None,
+          profile = Json.obj(),
+          token = Json.obj("biscuit" -> biscuitToken.serialize_b64url()),
+          realm = s"BiscuitUserExtractor@${ctx.route.serviceDescriptor.id}",
+          authConfigId = s"BiscuitUserExtractor@${ctx.route.serviceDescriptor.id}",
+          createdAt = DateTime.now(),
+          expiredAt = DateTime.now().plusHours(1),
+          lastRefresh = DateTime.now(),
+          tags = Seq.empty,
+          metadata = Map.empty,
+          location = ctx.route.serviceDescriptor.location
+        )
+        ctx.attrs.put(otoroshi.plugins.Keys.UserKey -> user)
+        Done.right.vfuture
+      }
+      case _ => unauthorized(Json.obj("error" -> "unauthorized", "error_description" -> "Bad user extraction, user id or username not valid"))
+    }
+  }
+
+  def unauthorized(error: JsObject): Future[Either[NgPreRoutingError, Done]] = {
+    NgPreRoutingErrorWithResult(Results.Unauthorized(error)).leftf
+  }
+
+  def handleError(message: String): Future[Either[NgPreRoutingError, Done]] =
+    NgPreRoutingErrorWithResult(Results.InternalServerError(Json.obj("error" -> message))).leftf
 }
