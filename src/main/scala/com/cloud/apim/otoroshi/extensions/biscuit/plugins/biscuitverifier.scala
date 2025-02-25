9c27d20b5c07d474b9e4a17cba7f997b1375f9f5
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-02-25 09:14:17 +0100

    add enforce back

--- src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/biscuitverifier.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/biscuitverifier.scala
@@ -152,7 +152,7 @@ class BiscuitTokenValidator extends NgAccessValidator {
             case None if !hasFailed && !config.enforce => NgAllowed.vfuture
             case Some(head) => {
               head.verify(ctx.request, Some(VerificationContext(ctx.route, ctx.request, ctx.user, ctx.apikey))) flatMap {
-                case Left(err) if err == "no token" => next(items.tail) // TODO: log error
+                case Left(err) if err == "no token" => next(items.tail)
                 case Left(err) =>
                   hasFailed = true
                   next(items.tail) // TODO: log error
