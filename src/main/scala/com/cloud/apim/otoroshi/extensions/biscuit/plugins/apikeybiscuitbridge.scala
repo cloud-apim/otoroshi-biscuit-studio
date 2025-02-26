9833f1b8148cf4c1a326f5a1870b001de974d862
SelligtomGit86 <thomas.delafaye@serli.com>
2025-02-26 11:27:01 +0100

    update apikey bridge plugin

--- src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/apikeybiscuitbridge.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/plugins/apikeybiscuitbridge.scala
@@ -19,16 +19,17 @@ import scala.jdk.CollectionConverters.{asScalaBufferConverter, asScalaSetConvert
 import scala.util.{Failure, Success, Try}
 
 case class BiscuitApiKeyBridgeConfig(
-    keypairRef: String,
+    keypairRef: String = "",
     enforce: Boolean = true,
-    extractorType: String,
-    extractorName: String
+    extractorType: String = "header",
+    extractorName: String = "Authorization",
+    clientIdKey: String = "client_id"
 ) extends NgPluginConfig {
   def json: JsValue = BiscuitApiKeyBridgeConfig.format.writes(this)
 }
 
 object BiscuitApiKeyBridgeConfig {
-  val configFlow: Seq[String] = Seq("keypair_ref", "enforce", "extractor_type", "extractor_name")
+  val configFlow: Seq[String] = Seq("keypair_ref", "enforce", "extractor_type", "extractor_name", "client_id_key")
   def configSchema(name: String): Option[JsObject] = Some(Json.obj(
     "keypair_ref" -> Json.obj(
       "type" -> "select",
@@ -60,13 +61,12 @@ object BiscuitApiKeyBridgeConfig {
     "extractor_name" -> Json.obj(
       "type" -> "text",
       "label" -> "Biscuit field name"
+    ),
+    "client_id_key" -> Json.obj(
+      "type" -> "text",
+      "label" -> "Client Id Key"
     )
   ))
-  val default = BiscuitApiKeyBridgeConfig(
-    keypairRef = "",
-    extractorType = "header",
-    extractorName = "Authorization"
-  )
 
   val format = new Format[BiscuitApiKeyBridgeConfig] {
     override def writes(o: BiscuitApiKeyBridgeConfig): JsValue = Json.obj(
@@ -74,13 +74,15 @@ object BiscuitApiKeyBridgeConfig {
       "enforce" -> o.enforce,
       "extractor_type" -> o.extractorType,
       "extractor_name" -> o.extractorName,
+      "client_id_key" -> o.clientIdKey
     )
     override def reads(json: JsValue): JsResult[BiscuitApiKeyBridgeConfig] = Try {
       BiscuitApiKeyBridgeConfig(
         keypairRef = json.select("keypair_ref").asOpt[String].getOrElse(""),
         enforce = json.select("enforce").asOpt[Boolean].getOrElse(true),
         extractorType = json.select("extractor_type").asOpt[String].getOrElse(""),
-        extractorName = json.select("extractor_name").asOpt[String].getOrElse("")
+        extractorName = json.select("extractor_name").asOpt[String].getOrElse(""),
+        clientIdKey = json.select("client_id_key").asOpt[String].getOrElse("")
       )
     } match {
       case Failure(exception) => JsError(exception.getMessage)
@@ -93,7 +95,7 @@ class BiscuitApiKeyBridgePlugin extends NgPreRouting {
   private val logger = Logger("biscuit-apikey-bridge-plugin")
   override def name: String = "Cloud APIM - Biscuit ApiKey bridge"
   override def description: Option[String] = "This plugin validates a Biscuit token".some
-  override def defaultConfigObject: Option[NgPluginConfig] = Some(BiscuitApiKeyBridgeConfig.default)
+  override def defaultConfigObject: Option[NgPluginConfig] = Some(BiscuitApiKeyBridgeConfig())
   override def core: Boolean = false
   override def noJsForm: Boolean = true
   override def configFlow: Seq[String] = BiscuitApiKeyBridgeConfig.configFlow
@@ -109,7 +111,7 @@ class BiscuitApiKeyBridgePlugin extends NgPreRouting {
     ().vfuture
   }
   def extractApiKey(ctx: NgPreRoutingContext, biscuitToken: Biscuit, config: BiscuitApiKeyBridgeConfig)(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
-    val otoroshiClientID = biscuitToken.authorizer().query("api_key_client_id($id) <- client_id($id)")
+    val otoroshiClientID = biscuitToken.authorizer().query(s"api_key_client_id($$id) <- ${config.clientIdKey}($$id)")
 
     val client_id: Option[String]   = Try(otoroshiClientID).toOption
       .map(_.asScala)
@@ -150,7 +152,7 @@ class BiscuitApiKeyBridgePlugin extends NgPreRouting {
                 ctx: NgPreRoutingContext
               )(implicit env: Env, ec: ExecutionContext): Future[Either[NgPreRoutingError, Done]] = {
 
-    val config = ctx.cachedConfig(internalName)(BiscuitApiKeyBridgeConfig.format).getOrElse(BiscuitApiKeyBridgeConfig.default)
+    val config = ctx.cachedConfig(internalName)(BiscuitApiKeyBridgeConfig.format).getOrElse(BiscuitApiKeyBridgeConfig())
     env.adminExtensions.extension[BiscuitExtension].flatMap(_.states.keypair(config.keypairRef)) match {
       case None => handleError("keypair_ref not found")
       case Some(keypair) => {
