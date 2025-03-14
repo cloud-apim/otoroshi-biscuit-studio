7fb010a816fa459984644e76ae0da680c79047e6
SelligtomGit86 <thomas.delafaye@serli.com>
2025-03-14 11:17:41 +0100

    Update Tokens inspector UI

--- src/main/resources/cloudapim/extensions/biscuit/playground/GraphicalTokensInspector.js
+++ src/main/resources/cloudapim/extensions/biscuit/playground/GraphicalTokensInspector.js
@@ -7,7 +7,7 @@ class GraphicalTokensInspector extends Component {
       pubKey: null,
       verifierRef: null,
       errorMessage: null,
-      loadedFacts: []
+      loadedFacts: [],
     };
   }
 
@@ -24,12 +24,11 @@ class GraphicalTokensInspector extends Component {
   );
 
   resetAll = () => {
-    this.setState({ forgeRef: null, verifierRef: null, loadedFacts: [] })
-  }
+    this.setState({ forgeRef: null, verifierRef: null, loadedFacts: [], token: null, pubKey: null });
+  };
 
   generateNewToken = () => {
     if (!this.state.forgeRef) {
-      console.log("forge is null");
       return;
     }
 
@@ -39,17 +38,16 @@ class GraphicalTokensInspector extends Component {
         method: "POST",
         credentials: "include",
         headers: {
-          "Accept": "application/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
       }
     )
-      .then((d) => d.json())
+      .then((res) => res.json())
       .then((data) => {
         if (!data?.done) {
           this.setState({
-            isReqLoading: false,
-            errorMessage: `Something went wrong : ${data.error}`,
+            errorMessage: `Something went wrong: ${data.error}`,
           });
         } else {
           this.setState({
@@ -61,116 +59,87 @@ class GraphicalTokensInspector extends Component {
   };
 
   loadVerifierFacts = () => {
-    fetch(`/extensions/cloud-apim/extensions/biscuit/tokens/verifiers/${this.state.verifierRef}/_datalog`,
-    {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((d) => d.json())
-    .then((data) => {
-      if (!data?.done) {
-        this.setState({
-          isReqLoading: false,
-          errorMessage: `Something went wrong : ${data.error}`,
-        });
-      } else {
-        this.setState({ loadedFacts: data?.datalog || [] })
+    fetch(
+      `/extensions/cloud-apim/extensions/biscuit/tokens/verifiers/${this.state.verifierRef}/_datalog`,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
       }
-    });
-
-  }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data?.done) {
+          this.setState({
+            errorMessage: `Something went wrong: ${data.error}`,
+          });
+        } else {
+          this.setState({ loadedFacts: data?.datalog || [] });
+        }
+      });
+  };
 
   render() {
     return React.createElement(
       "div",
       { className: "container py-5 text-center" },
-      React.createElement(
-        "h1",
-        { className: "mb-4 fw-bold text-primary" },
-        "Graphical Tokens Inspector"
-      ),
+      React.createElement("h1", { className: "mb-4 fw-bold text-white bg-primary p-3 rounded" }, "Graphical Tokens Inspector"),
       React.createElement(
         "div",
-        { className: "mb-3" },
-        React.createElement(SelectInput, {
-          label: "Use a token forge",
-          value: this.state.forgeRef,
-          onChange: (forgeRef) => {
-            console.log("forgeRef = ", forgeRef);
-            this.forgesClient.findById(forgeRef).then((forgeRefById) =>
-              console.log("forgeRefById = ", forgeRefById)
-            );
-            this.setState({ forgeRef });
-          },
-          valuesFrom:
-            "/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-forges",
-          transformer: (item) => ({ label: item.name, value: item.id }),
-        })
-      ),
-      this.state.forgeRef &&
-      React.createElement(
-        "button",
-        {
-          className: "btn btn-success d-flex align-items-center mx-auto px-4 py-2",
-          onClick: this.generateNewToken,
-        },
-        React.createElement("i", { className: "fas fa-cogs me-2 fs-5" }),
-        "Generate Token"
-      ),
-      React.createElement(
-        "div",
-        { className: "mb-3" },
-        React.createElement(SelectInput, {
-          label: "Use a verifier",
-          value: this.state.verifierRef,
-          onChange: (verifierRef) => {
-            this.setState({ verifierRef });
-          },
-          valuesFrom:
-            "/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-verifiers",
-          transformer: (item) => ({ label: item.name, value: item.id }),
-        })
-      ),
-      this.state.verifierRef &&
-      React.createElement(
-        "button",
-        {
-          className: "btn btn-success d-flex align-items-center mx-auto px-4 py-2",
-          onClick: () => this.loadVerifierFacts(),
-        },
-        React.createElement("i", { className: "fas fa-cogs me-2 fs-5" }),
-        "Load verifier facts"
-      ),
-
-      React.createElement(
-        "button",
-        {
-          className: "btn btn-success d-flex align-items-center mx-auto px-4 py-2",
-          onClick: this.resetAll,
-        },
-        React.createElement("i", { className: "fas fa-danger me-2 fs-5" }),
-        "Reset all"
-      ),
-
-      React.createElement(
-        "div",
-        { className: "row justify-content-center mt-4" },
+        { className: "row justify-content-center" },
         React.createElement(
           "div",
-          { className: "col-lg-6 col-md-8 col-sm-10 bg-light p-4 rounded shadow" },
+          { className: "col-md-6 mb-4" },
+          React.createElement("div", { className: "card shadow p-3 bg-white rounded" },
+            React.createElement("div", { className: "card-body" },
+              React.createElement(SelectInput, {
+                label: "Use a token forge",
+                value: this.state.forgeRef,
+                onChange: (forgeRef) => this.setState({ forgeRef }),
+                valuesFrom: "/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-forges",
+                transformer: (item) => ({ label: item.name, value: item.id }),
+              })
+            )
+          )
+        ),
+        React.createElement(
+          "div",
+          { className: "col-md-6 mb-4" },
+          React.createElement("div", { className: "card shadow p-3 bg-white rounded" },
+            React.createElement("div", { className: "card-body" },
+              React.createElement(SelectInput, {
+                label: "Use a verifier",
+                value: this.state.verifierRef,
+                onChange: (verifierRef) => this.setState({ verifierRef }),
+                valuesFrom: "/bo/api/proxy/apis/biscuit.extensions.cloud-apim.com/v1/biscuit-verifiers",
+                transformer: (item) => ({ label: item.name, value: item.id }),
+              })
+            )
+          )
+        )
+      ),
+      this.state.forgeRef &&
+      React.createElement("button", { className: "btn btn-primary mb-3 px-4 py-2", onClick: this.generateNewToken },
+        React.createElement("i", { className: "fas fa-cogs me-2" }), "Generate Token"
+      ),
+      this.state.verifierRef &&
+      React.createElement("button", { className: "btn btn-info mb-3 px-4 py-2", onClick: this.loadVerifierFacts },
+        React.createElement("i", { className: "fas fa-cogs me-2" }), "Load Verifier Facts"
+      ),
+      React.createElement("button", { className: "btn btn-danger px-4 py-2", onClick: this.resetAll },
+        React.createElement("i", { className: "fas fa-trash-alt me-2" }), "Reset All"
+      ),
+      React.createElement("div", { className: "row justify-content-center mt-4" },
+        React.createElement("div", { className: "col-lg-6 col-md-8 col-sm-10 bg-light p-4 rounded shadow" },
           React.createElement("bc-token-printer", {
             showauthorizer: true,
             readonly: true,
-            rootPublicKey: this.state?.pubKey,
-            biscuit: this.state?.token,
-            authorizer: this.state?.loadedFacts
-              .map((line) => line.trim())
-              .join("\n"),
+            rootPublicKey: this.state.pubKey,
+            biscuit: this.state.token,
+            authorizer: this.state.loadedFacts.map((line) => line.trim()).join("\n"),
           })
         )
       )
