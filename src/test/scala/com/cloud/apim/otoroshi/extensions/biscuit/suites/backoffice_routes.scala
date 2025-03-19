b4372da687c7f3834ce4c51ec97ec48c84e9eef6
SelligtomGit86 <thomas.delafaye@serli.com>
2025-03-19 16:08:17 +0100

    add test to verify a token with backoffice route

--- src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/backoffice_routes.scala
+++ src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/backoffice_routes.scala
@@ -252,4 +252,71 @@ class BackofficeRoutesSuite extends BiscuitStudioOneOtoroshiServerPerSuite {
     assertEquals(encodedBiscuit.authorizer().facts().size(), tokenConfig.facts.length + 4, s"generated token doesn't contain all facts")
     assertEquals(encodedBiscuit.authorizer().checks().asScala.flatMap(_._2.asScala).size, tokenConfig.checks.length, s"generated token doesn't contain all checks")
   }
+
+  test("should be able to verify a token  with a forge_ref and body config") {
+    val biscuitKeyPair = new KeyPair()
+    val keypair = BiscuitKeyPair(
+      id = IdGenerator.namedId("biscuit-keypair", otoroshi.env),
+      name = "New Biscuit Key Pair",
+      description = "New biscuit KeyPair",
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      privKey = biscuitKeyPair.toHex,
+      pubKey = biscuitKeyPair.public_key().toHex
+    )
+
+    val forge = BiscuitTokenForge(
+      id = IdGenerator.namedId("biscuit-forge", otoroshi.env),
+      name = "New biscuit token",
+      description = "New biscuit token",
+      keypairRef = keypair.id,
+      metadata = Map.empty,
+      tags = Seq.empty,
+      location = EntityLocation.default,
+      config = BiscuitForgeConfig(
+        facts = Seq(
+          "name(\"otoroshi-biscuit-studio-test\")",
+          "user(\"biscuit-test-user\")",
+          "role(\"user\")"
+        ),
+        checks = Seq(
+          "check if server(\"biscuit-server-test\")",
+          "check if operation(\"read\")"
+        )
+      )
+    )
+
+    val verifierConfig = VerifierConfig(
+      checks = Seq(
+        "check if name(\"otoroshi-biscuit-studio-test\")",
+        "check if user(\"biscuit-test-user\")",
+        "check if role(\"user\")"
+      ),
+      facts = Seq(
+        "server(\"biscuit-server-test\")",
+        "operation(\"read\")"
+      )
+    )
+
+    /// Create entities
+    client.forEntity("biscuit.extensions.cloud-apim.com", "v1", "biscuit-keypairs").upsertEntity(keypair)
+    client.forEntity("biscuit.extensions.cloud-apim.com", "v1", "biscuit-forges").upsertEntity(forge)
+    await(5.seconds)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  test Good biscuit verifier                                    ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val goodVerifier = client.call("POST", s"http://otoroshi.oto.tools:${port}/extensions/cloud-apim/extensions/biscuit/tokens/verifiers/_test", Map(
+      "Content-Type" -> s"application/json"
+    ), Some(Json.obj(
+      "forge_ref" -> forge.id,
+      "keypair_ref"-> keypair.id,
+      "config" -> verifierConfig.json
+    ))).awaitf(5.seconds)
+
+    assert(goodVerifier.json.at("status").isDefined, "status for good verifier should be defined")
+    assert(goodVerifier.json.at("message").isDefined, "message for good verifier should be defined")
+    assertEquals(goodVerifier.json.at("status").asString, "success", "status message should be success")
+    assertEquals(goodVerifier.json.at("message").asString, "Checked successfully", "message should be 'Checked successfully'")
+  }
 }
