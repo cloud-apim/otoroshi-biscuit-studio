18bdd6830593258a25e90b967ea77c0be9200ee2
SelligtomGit86 <thomas.delafaye@serli.com>
2025-03-19 15:16:37 +0100

    add test to generate a biscuit token from backoffice route with public and private keys + config

--- src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/backoffice_routes.scala
+++ src/test/scala/com/cloud/apim/otoroshi/extensions/biscuit/suites/backoffice_routes.scala
@@ -1,17 +1,14 @@
 package com.cloud.apim.otoroshi.extensions.biscuit.suites
 
 import com.cloud.apim.otoroshi.extensions.biscuit.BiscuitStudioOneOtoroshiServerPerSuite
-import com.cloud.apim.otoroshi.extensions.biscuit.entities.BiscuitKeyPair
+import com.cloud.apim.otoroshi.extensions.biscuit.entities.{BiscuitForgeConfig, BiscuitKeyPair}
 import org.biscuitsec.biscuit.crypto.KeyPair
-import otoroshi.models.EntityLocation
-import otoroshi.next.models._
-import otoroshi.security.IdGenerator
+import org.biscuitsec.biscuit.token.Biscuit
 import otoroshi.utils.syntax.implicits._
-import otoroshi_plugins.com.cloud.apim.otoroshi.extensions.biscuit.plugins.{BiscuitExposePubKeysPluginConfig, ExposeBiscuitPublicKeysPlugin}
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.Json
 
-import java.util.UUID
 import scala.concurrent.duration.DurationInt
+import scala.jdk.CollectionConverters._
 
 class BackofficeRoutesSuite extends BiscuitStudioOneOtoroshiServerPerSuite {
 
@@ -29,6 +26,52 @@ class BackofficeRoutesSuite extends BiscuitStudioOneOtoroshiServerPerSuite {
     assert(resp.json.at("done").asBoolean, s"request 'done' status should be true")
     assert(resp.json.at("pubKey").isDefined, "'pubKey' should be defined")
     assert(resp.json.at("privKey").isDefined, "'privKey' should be defined")
+  }
+
+  test("should be able to generate a token with public and private keys") {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                       test endpoint                                            ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val keypair = new KeyPair()
+
+    val tokenConfig = BiscuitForgeConfig(
+      facts = Seq(
+        "name(\"otoroshi-biscuit-studio-test\")",
+        "user(\"biscuit-test-user\")",
+        "role(\"user\")"
+      ),
+      checks = Seq(
+        "check if user(\"biscuit-test-user\")",
+        "check if role(\"user\")"
+      )
+    )
+
+    val resp = client.call("POST", s"http://otoroshi.oto.tools:${port}/extensions/cloud-apim/extensions/biscuit/tokens/_generate",
+      Map(
+        "Content-Type" -> s"application/json"
+      ),
+      Some(
+        Json.obj(
+          "pubKey" -> keypair.public_key().toHex,
+          "privKey" -> keypair.toHex,
+          "config" -> tokenConfig.json
+          )
+        )
+    ).awaitf(5.seconds)
+    assertEquals(resp.status, 200, s"verifier route did not respond with 200")
+    assert(resp.json.at("done").isDefined, s"'done' field should be defined")
+    assert(resp.json.at("done").asBoolean, s"request 'done' status should be true")
+    assert(resp.json.at("pubKey").isDefined, "'pubKey' should be defined")
+    assertEquals(resp.json.at("pubKey").asString, keypair.public_key().toHex, "public key response should match to given public key")
+    assert(resp.json.at("token").isDefined, "'token' should be defined")
+
+    assert(resp.json.at("token").isDefined, "token should be successfully generated")
+
+    val genToken = resp.json.at("token").asString
+    val encodedBiscuit = Biscuit.from_b64url(genToken, keypair.public_key())
+
+    assertEquals(encodedBiscuit.authorizer().facts().size(), tokenConfig.facts.length, s"generated token doesn't contain all facts")
+    assertEquals(encodedBiscuit.authorizer().checks().asScala.flatMap(_._2.asScala).size, tokenConfig.checks.length, s"generated token doesn't contain all checks")
   }
 
 }
