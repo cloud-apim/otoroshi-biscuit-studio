a44730be62426df7e845902addcc6d805c0583fa
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-06-03 16:28:41 +0200

    Use otoroshi 17.3.0

--- src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/workflows.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/biscuit/workflows.scala
@@ -1,7 +1,7 @@
 package otoroshi_plugins.com.cloud.apim.otoroshi.extensions.biscuit
 
 import otoroshi.env.Env
-import otoroshi.next.workflow.{WorkflowError, WorkflowFunction}
+import otoroshi.next.workflow.{WorkflowError, WorkflowFunction, WorkflowRun}
 import otoroshi.utils.TypedMap
 import otoroshi.utils.syntax.implicits._
 import play.api.libs.json._
@@ -17,14 +17,14 @@ object WorkflowFunctionsInitializer {
 }
 
 class BiscuitVerifyFunction extends WorkflowFunction {
-  override def call(args: JsObject)(implicit env: Env, ec: ExecutionContext): Future[Either[WorkflowError, JsValue]] = {
+  override def callWithRun(args: JsObject)(implicit env: Env, ec: ExecutionContext, wfr: WorkflowRun): Future[Either[WorkflowError, JsValue]] = {
     val verifier = args.select("verifier").asString
     val token = args.select("token").asString
     val extension = env.adminExtensions.extension[BiscuitExtension].get
     extension.states.biscuitVerifier(verifier) match {
       case None => WorkflowError(s"biscuit verifier not found", Some(Json.obj("verifier_id" -> verifier)), None).leftf
       case Some(verifier) => {
-        verifier.verifyBase64Token(token, None, TypedMap.empty) // TODO: create verification context from attrs when otoroshi 17.3.0
+        verifier.verifyBase64Token(token, None, wfr.attrs)
           .map {
             case Left(error) => Json.obj("success" -> false, "error" -> error).right
             case Right(_) => Json.obj("success" -> true, "error" -> JsNull).right
